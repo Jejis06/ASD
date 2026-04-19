868ea09bd248a76056069965a2e2d09742fb7fd5
Ignacy Buczek <wertex3233@gmail.com>
2026-04-19 21:45:20 +0200

    zadadadad

--- cw5/7.min-toll-path.py
+++ cw5/7.min-toll-path.py
@@ -13,17 +13,35 @@ graph_neighbor = list[list[tuple[int,int]]]
 
 # wagi (0,1)
 
+# O (V + E)
 def find_cheapes_path(G:graph_neighbor, s:int, t:int) -> list[int]:
     n = len(G)
     parent:list[int] = [-1 for _ in range(n)]
-    dist:list[int]   = [ -1 for _ in range(n)]
+    dist:list[int]   = [ float('inf') for _ in range(n)]
 
 
     q: deque[int] = deque([s])
+    dist[s] = 0
 
+    while q:
+        vert = q.popleft()
+        if vert == t: break
 
+        for child, w in G[vert]:
+            if dist[vert] + w < dist[child]:
+                dist[child] = dist[vert] + w
+                parent[child] = vert
 
+                if w == 0: q.appendleft(child)
+                else: q.append(child)
 
+    if dist[t] == float('inf'):
+        return []
+    path = [t]
+    while t != s:
+        t = parent[t]
+        path.append(t)
+    return path[::-1]
 
 def solution():
     # TODO: Implement algorithm logic here
